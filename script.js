2b1c72ba1ba0768e238275bffebd8cf86449e82e
Omar Hersi <o.hersi@yahoo.com>
2021-12-22 15:44:38 -0500

    added enemy block, some game functionalities

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-//Sonic should have health (100hp)
+
 //Speed up blocks after reaching certain score; new Level
-//Track/display score - levels
-//Have blocks randomly appearing moving down the page
-//Collision (change gif on impact)
 
 //DOM Queries
 const sonic = document.querySelector('#sonic');
-const enemy = document.querySelector('#obstacle');
+const enemy = document.querySelector('#obstacles');
+const manyEnemies = document.querySelector('.blocks')
+const score = document.querySelector('#score');
+const levels = document.querySelector('#levels');
+const healthbar = document.querySelector('#healthbar')
 
 
+//Track/display score - levels
+let counter = 0;
+let level = 1;
+//Sonic should have health (100hp)
+let Health = 100;
+//speed of enemy
+let speed = 1.7;
+// let speed = window.getComputedStyle(enemy).getPropertyValue("animation-duration");
+// console.log(`Intial speed is ${speed}`);
 
 
 // Have Sonic run - Move Left , Right, Up and Down//
@@ -62,3 +72,75 @@ document.addEventListener('keydown', event => {
     }
 })
 
+//Have blocks randomly appearing moving down the page
+enemy.addEventListener('animationiteration', () => {
+    let random = Math.floor(Math.random() * 6);
+    left = random * 100;
+    enemy.style.left =  left + 'px'
+    counter++;
+    //right now score is tied to the blocks appearence, should just be a timer
+    score.textContent = `Score: ${counter}`;
+    speedUp();
+    updateLevel();
+    addBlock();
+    
+})
+
+//speed up blocks
+speedUp = () => {
+    if (counter % 10 === 0 && speed >= 0) {
+        console.log("10+");
+        // speed -= 0.2;
+        console.log(speed);
+        enemy.style.animationDuration = speed + 's';
+    }
+}
+
+//update Level
+updateLevel = () => {
+    if(counter % 10 === 0) {
+        level++;
+        levels.textContent = `Level: ${level}`
+        console.log("LEVEL UP")
+    }
+}
+
+//add Enemies 
+
+addBlock = () => {
+    if(counter % 10 === 0 && counter < 100) {
+        // manyEnemies.appendChild(enemy.cloneNode(true))
+    }
+}
+
+
+//remove health 
+removeHealth = () => {
+    Health -= 10;
+    if(Health >= 0) {
+        healthbar.style.width = Health + '%'
+        healthbar.innerHTML = Health + 'HP'
+        sonic.style.backgroundImage = "url(images/sonic-hit.gif)"
+    }
+}
+//End the game
+gameOver = () => {
+    alert(`Game Over! Final score is ${counter}.`)
+        enemy.style.animation = 'none'; 
+}
+
+
+//Collision (change gif on impact)
+setInterval( () => {
+    let sonicLeft = parseInt(window.getComputedStyle(sonic).getPropertyValue("left"))
+    // console.log(sonicLeft);
+    let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"))
+    // console.log(enemyLeft);
+    let enemyTop = parseInt(window.getComputedStyle(enemy).getPropertyValue("top"))
+    // console.log(enemyTop)
+    if(sonicLeft === enemyLeft && enemyTop < 500 && enemyTop > 300){
+        console.log("HIT");
+        removeHealth();
+            if (Health <= 0) gameOver();
+    }
+}, 200);
